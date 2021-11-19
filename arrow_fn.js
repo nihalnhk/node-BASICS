6684eb3c0da243d757e30558c2ce004c94849fd8
nihalnhk <k.nihal37@gmail.com>
2021-11-19 23:23:56 +0530

    cls and arrow fn

---
+++ arrow_fn.js
@@ -0,0 +1,10 @@
+// let add = (a, b) => a + b;
+
+// console.log(add(10, 20));
+
+let greater = (a, b) => {
+  if (a > b) console.log("a is greater");
+  else console.log("b is greater");
+};
+
+greater(2, 10);
--- es6_class.js
+++ es6_class.js
@@ -1,14 +1,30 @@
-class c1 {
-  constructor(n1, n2) {
-    this.n1 = n1;
-    this.n2 = n2;
-  }
+// class c1 {
+//   constructor(n1, n2) {
+//     this.n1 = n1;
+//     this.n2 = n2;
+//   }
 
-  m1() {
-    console.log("numbers are :" + this.n1 + " " + this.n2);
+//   m1() {
+//     console.log("numbers are :" + this.n1 + " " + this.n2);
+//   }
+// }
+
+// let o1 = new c1(2, 4);
+
+// o1.m1();
+
+class c1 {
+  dis() {
+    console.log("from c1");
   }
 }
 
-let o1 = new c1(2, 4);
+class c2 extends c1 {
+  disp() {
+    console.log("from c2");
+  }
+}
 
-o1.m1();
+var o1 = new c2();
+o1.disp();
+o1.dis();
